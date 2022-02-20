0e41eea11c91ed89c2d5617533d6fc7237af1705
newvid13 <100053935+newvid13@users.noreply.github.com>
2022-02-20 02:40:10 +0100

    Add files via upload

---
+++ Unit_Base.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitState
+{
+    Default,
+    Retreat,
+    Captured
+}
+
+public class Unit_Base : MonoBehaviour
+{
+    public float aiPriority;
+    public UnitState myState = UnitState.Default;
+
+    public int water;
+    public float waterOffset;
+    public Transform objWater;
+    public SpriteRenderer sprWater;
+    public Vector3 waterPos;
+    public Color waterBlue, waterRed;
+
+    public Unit_Move scrMove;
+    public Transform objManager;
+    public Main_Manager scrManager;
+    public Transform visBody;
+    public MeshRenderer visionCone;
+
+    public AudioSource myAudio;
+    public AudioClip[] audioClips;
+
+    public virtual void Start()
+    {
+        sprWater = objWater.GetComponent<SpriteRenderer>();
+        waterPos = objWater.transform.localPosition;
+        scrMove = GetComponent<Unit_Move>();
+        scrManager = objManager.GetComponent<Main_Manager>();
+        myAudio = GetComponentInChildren<AudioSource>();
+
+        UpdateWater();
+        InvokeRepeating("Thirst", 1f, 1f);
+    }
+
+    public virtual void Drink()
+    {
+        water += 5;
+        UpdateWater();
+    }
+
+    public virtual void Thirst()
+    {
+        if(myState == UnitState.Default)
+        {
+            water-=3;
+            UpdateWater();
+        }
+    }
+
+    public virtual void UpdateWater()
+    {
+        water = Mathf.Clamp(water, -100, 101);
+
+        if (water < 0)
+        {
+            sprWater.color = waterRed;
+            waterPos.y = -4.3f + (float)water * -waterOffset;
+        }
+        else
+        {
+            sprWater.color = waterBlue;
+            waterPos.y = -4.3f + (float)water * waterOffset;
+        }
+
+        objWater.transform.localPosition = waterPos;
+
+        if (water < -99)
+        {
+            if(aiPriority < 4)
+            {
+                scrManager.RemoveUnit(transform);
+            }
+
+            scrMove.Die();
+        }
+    }
+
+    public virtual void Damage(int dmg)
+    {
+        water -= dmg;
+        UpdateWater();
+    }
+
+    public virtual int PathCalc()
+    {
+        int reWater = 100 + water;
+        return reWater;
+    }
+
+    public void PrisonRelease()
+    {
+        myState = UnitState.Default;
+        transform.tag = "Player";
+        visBody.gameObject.layer = 0;
+
+        if (aiPriority < 4 && aiPriority != 0)
+        {
+            scrManager.AddUnit(transform);
+            visionCone.enabled = true;
+        }
+    }
+
+    public void PlayS(int c)
+    {
+        scrManager.SoundUI(c);
+    }
+
+    public void UnitSound(int type)
+    {
+        switch (type)
+        {
+            case 1:
+                //move
+                myAudio.pitch = Random.Range(0.95f, 1.05f);
+                myAudio.PlayOneShot(audioClips[Random.Range(0,2)]);
+                break;
+            case 2:
+                //fire
+                myAudio.pitch = Random.Range(0.95f, 1f);
+                myAudio.PlayOneShot(audioClips[2]);
+                break;
+            case 3:
+                //special
+                myAudio.pitch = Random.Range(0.95f, 1.05f);
+                myAudio.PlayOneShot(audioClips[3]);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Unit_Move.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Unit_Move : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+    public Color lineWhite, lineRed;
+
+    private bool isDrawing = false, isMoving = false;
+    private LineRenderer lineRend;
+    public SpriteRenderer arrowRend;
+    public SpriteRenderer glowRend;
+
+    private List<Vector3> linePoints = new List<Vector3>();
+    private Queue<Vector3> lineQueue = new Queue<Vector3>();
+    private Unit_Base scrAI;
+    private float pathLength;
+
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        lineRend = GetComponentInChildren<LineRenderer>();
+        scrAI = GetComponent<Unit_Base>();
+    }
+
+    void Update()
+    {
+        if (isDrawing)
+        {
+            Vector3 mousePosRaw = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePos = new Vector3(mousePosRaw.x, 1f, mousePosRaw.z);
+
+            if (DistancePoint(mousePos) > 0.3f)
+            {
+                linePoints.Add(mousePos);
+                lineRend.positionCount = linePoints.Count();
+                lineRend.SetPositions(linePoints.ToArray());
+
+                pathLength += 0.3f;
+            }
+        }
+
+        if(isMoving && MoveOn())
+        {
+            myAgent.SetDestination(lineQueue.Dequeue());
+        }
+    }
+
+    private float DistancePoint(Vector3 point)
+    {
+        PathLimit();
+
+        if(linePoints.Count == 0)
+        {
+            float dist = Vector3.Distance(transform.position, point);
+            if(dist < 1.5f)
+            {
+                return 0f;
+            }
+        }
+
+        if (linePoints.Any())
+        {
+            return Vector3.Distance(point, linePoints.Last());
+        }
+        else
+        {
+            return 5f;
+        }
+    }
+
+    private bool MoveOn()
+    {
+        if (lineQueue.Count == 0)
+        {
+            isMoving = false;
+            DrawOff();
+            return false;
+        }
+        else if(myAgent.hasPath == false || myAgent.remainingDistance < 0.4f)
+        {
+            lineRend.SetPositions(lineQueue.ToArray());
+            return true;
+        }
+
+        return false;
+    }
+
+    public void StartDraw()
+    {
+        DrawOff();
+        pathLength = 0f;
+        lineRend.endColor = lineWhite;
+        lineRend.enabled = true;
+        isDrawing = true;
+        isMoving = false;
+
+        myAgent.SetDestination(transform.position);
+        myAgent.isStopped = true;
+    }
+
+    public void EndDraw()
+    {
+        isDrawing = false;
+        NavMeshPath path = new NavMeshPath();
+
+        if (linePoints.Count > 1)
+        {
+            myAgent.CalculatePath(linePoints.Last(), path);
+            lineRend.Simplify(0.5f);
+            linePoints.Clear();
+
+            for (int i = 0; i < lineRend.positionCount; i++)
+            {
+                linePoints.Add(lineRend.GetPosition(i));
+            }
+
+            SetArrow();
+            lineQueue = new Queue<Vector3>(linePoints);
+            isMoving = true;
+            myAgent.isStopped = false;
+
+            scrAI.UnitSound(1);
+        }
+        else
+        {
+            DrawOff();
+        }
+
+    }
+
+    private void DrawOff()
+    {
+        lineRend.positionCount = 1;
+        linePoints.Clear();
+        lineQueue.Clear();
+        lineRend.enabled = false;
+        arrowRend.enabled = false;
+    }
+
+    private void SetArrow()
+    {
+        int lastPoint = linePoints.Count;
+        Vector3 a1 = linePoints[lastPoint - 1];
+        Vector3 a2 = linePoints[lastPoint - 2];
+        Vector3 a3 = a1 - a2;
+
+        Quaternion arrowRot = Quaternion.identity;
+        arrowRot = Quaternion.LookRotation(a3);
+        arrowRot.eulerAngles = new Vector3(90, arrowRot.eulerAngles.y, arrowRot.eulerAngles.z + 90);
+
+        arrowRend.transform.rotation = arrowRot;
+        arrowRend.transform.position = linePoints[lastPoint - 1];
+        arrowRend.enabled = true;
+    }
+
+    private void PathLimit()
+    {
+        int reWater = scrAI.PathCalc();
+        float pathL = myAgent.speed * (reWater/3);
+
+        if(pathLength > pathL)
+        {
+            lineRend.endColor = lineRed;
+            arrowRend.color = lineRed;
+        }
+        else
+        {
+            lineRend.endColor = lineWhite;
+            arrowRend.color = lineWhite;
+        }
+    }
+
+    public void DirectMove(Vector3 destination)
+    {
+        DrawOff();
+        isMoving = false;
+        myAgent.isStopped = false;
+
+        myAgent.SetDestination(destination);
+    }
+
+    public void Die()
+    {
+        DrawOff();
+        Destroy(gameObject);
+    }
+}
